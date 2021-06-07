07ea0ad1e0c46f307823837be7f68ec31e8674de
Mia <vottus@vott.us>
2021-06-07 09:07:01 +0200

    chore: lint

---
+++ .eslintrc.js
@@ -0,0 +1,11 @@
+module.exprots = {
+  env: {
+    node: true,
+  },
+  parserOptions: {
+    ecmaVersion: "18",
+    sourceType: "module",
+  },
+  parser: "babel-eslint",
+  extends: "@sysdotini/eslint-config",
+};
