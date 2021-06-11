dd6a3388f62fed92574da7c474daafcc0251d259
Mia <vottus@vott.us>
2021-06-11 11:18:43 +0200

    fix: use FadeBox in HeroWrapper

--- src/components/elements/HeroWrapper.tsx
+++ src/components/elements/HeroWrapper.tsx
@@ -1,4 +1,3 @@
-import { motion } from "framer-motion";
 import { ComponentProps, PropsWithChildren } from "react";
 import { transition } from "../../tailwind";
 import { FadeBox } from "../motion/FadeBox";
@@ -10,15 +9,15 @@ const HeroWrapper = ({
   delayOrder,
   ...props
 }: PropsWithChildren<ComponentProps<"a"> & { hoverable?: boolean; delayOrder?: number }>) => (
-  <motion.div
-    // delayOrder={delayOrder}
+  <FadeBox
+    delayOrder={delayOrder}
     whileHover={hoverable ? { scale: 0.98 } : {}}
     // onViewportBoxUpdate={(e) => console.log(e.x)}
     className={`w-full h-full rounded-3xl py-12 px-16 flex flex-col justify-center cursor-pointer ${className} ${transition}`}
     {...props}
   >
     {children}
-  </motion.div>
+  </FadeBox>
 );
 
 export { HeroWrapper };
