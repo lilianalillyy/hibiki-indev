b1ebebd627ae2040b3bf39f522e37f4bcd533f9b
Mia <vottus@vott.us>
2021-06-11 12:48:15 +0200

    feat: animate invitehero buttons

--- src/pages/landing/sections/InviteHero.tsx
+++ src/pages/landing/sections/InviteHero.tsx
@@ -1,6 +1,7 @@
 import { HeroWrapper } from "../../../components/wrappers/HeroWrapper";
 import { LinkButton } from "../../../components/elements/Button";
 import { HeroTitle, HeroSubtitle } from "../../../components/typography/HeroTypography";
+import FadeBox from "../../../components/motion/FadeBox";
 
 const InviteHero = () => (
   <HeroWrapper viewOffset={1} hoverable={true} className={`bg-gradient-to-tr bg-gradient-2 from-primary to-primary text-white py-28`}>
@@ -10,17 +11,21 @@ const InviteHero = () => (
       <HeroSubtitle>1160 other server owners are already using Hibiki!</HeroSubtitle>
 
       <div className="mt-10 flex flex-col md:flex-row justify-center items-center">
-        <LinkButton type="white" to="/invite">
-          <span>Add Hibiki to your server</span>
-        </LinkButton>
-
-        <LinkButton type="white" to="/contribute">
-          <span>View Source on GitHub</span>
-        </LinkButton>
-
-        <LinkButton type="white" to="/vote">
-          <span>Vote for Hibiki on Top.gg</span>
-        </LinkButton>
+        <FadeBox viewOffset={1.7}>
+          <LinkButton type="white" to="/invite">
+            <span>Add Hibiki to your server</span>
+          </LinkButton>
+        </FadeBox>
+        <FadeBox viewOffset={1.8}>
+          <LinkButton type="white" to="/contribute">
+            <span>View Source on GitHub</span>
+          </LinkButton>
+        </FadeBox>
+        <FadeBox viewOffset={1.9}>
+          <LinkButton type="white" to="/vote">
+            <span>Vote for Hibiki on Top.gg</span>
+          </LinkButton>
+        </FadeBox>
       </div>
     </div>
   </HeroWrapper>
